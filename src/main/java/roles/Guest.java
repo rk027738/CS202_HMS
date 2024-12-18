505e842c874c0fb7d487f56702163d751db88a50
Abdul Rehman Khan <rehman.khan@ozu.edu.tr>
2024-12-18 17:57:09 +0300

    deleted commented methods

--- src/main/java/roles/Guest.java
+++ src/main/java/roles/Guest.java
@@ -4,8 +4,6 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
-import utils.DatabaseUtils.*;
-
 import static utils.DatabaseUtils.*;
 
 
